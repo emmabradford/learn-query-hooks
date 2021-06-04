ab3c1368171b7ad9be75b46724b8dcc58be0c390
EmmaAbe <emma.abe.bradford@gmail.com>
2021-06-04 14:51:33 -0500

    added some basic app code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import {Switch, Route, Redirect } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route path={"/"} exact component={}/>
-        <Route path={"/post/:id"} component={}/>
-        <Redirect to={"/"} />
-      </Switch>
+      <h1> Estar Wars </h1>
+      <div className="content"/>
     </div>
   );
 }
