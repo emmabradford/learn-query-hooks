0fd3885652f61f548b6d745aa41d7448c97d32ef
EmmaAbe <emma.abe.bradford@gmail.com>
2021-06-04 15:27:46 -0500

    added planets paige and person paige

---
+++ src/components/People.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const People = ()=>{
+    return(
+        <div>
+            <h2>Personas, Personas, Personas</h2>
+        </div>
+    );
+}
+
+export default People;
---
+++ src/components/Planets.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Planets = ()=>{
+    return(
+        <div>
+            <h2>Planetas, Planetas, Planetas</h2>
+        </div>
+    );
+}
+
+export default Planets;
