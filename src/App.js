ec309a3e686469209039ecb264599a612bf91431
EmmaAbe <emma.abe.bradford@gmail.com>
2021-06-04 16:18:29 -0500

    added query client provider

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import Planets from "./components/Planets";
 import People from "./components/People";
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   const [page, setPage] = useState('planets');
   return (
-    <div className="App">
-      <h1> Battalla en los estreyas </h1>
-      <Navbar setPage={setPage}/>
-      <div className="content">
-        {page === 'planets'?<Planets />:<People />};
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1> Battalla en los estreyas </h1>
+        <Navbar setPage={setPage}/>
+        <div className="content">
+          {page === 'planets'?<Planets />:<People />};
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
 
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 
+const fetchPlanets = async () => {
+    const res = await fetch("http://swapi.dev/api/planets/");
+    return res.json();
+}
 const Planets = ()=>{
+    const { data, status } = useQuery('planets', fetchPlanets);
+    console.log(data);
+
     return(
         <div>
             <h2>Planetas, Planetas, Planetas</h2>
