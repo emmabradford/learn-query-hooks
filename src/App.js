9d0cf675ac77bdbef51b21599b1fd6df411ee5a6
EmmaAbe <emma.abe.bradford@gmail.com>
2021-06-04 14:58:34 -0500

    added navbar to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <h1> Estar Wars </h1>
+      <Navbar/>
       <div className="content"/>
     </div>
   );
