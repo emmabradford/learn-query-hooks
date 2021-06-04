db872ba8a8d4bbb3780da3ee0ec14dff5e2be12f
EmmaAbe <emma.abe.bradford@gmail.com>
2021-06-04 17:28:42 -0500

    fleshed out people

--- src/components/People.js
+++ src/components/People.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Person from './Person';
+
+const fetchPeople = async () => {
+    const res = await fetch('http://swapi.dev/api/people/');
+    return res.json();
+}
 
 const People = ()=>{
+    const { data, status } =  useQuery('people', fetchPeople);
+    console.log(data);
+
     return(
         <div>
             <h2>Personas, Personas, Personas</h2>
+            {status === 'loading' && (
+                <div>Loading unadulterated loading ....</div>
+            )}
+
+            {status === 'errpr' && (
+                <div>No hay Personas</div>
+            )}
+
+            {status === 'success' && (
+                <div>
+                    { data.results.map(person => <Person key={person.name} person={person}/>)}
+                </div>
+            )}
+            
         </div>
     );
 }
