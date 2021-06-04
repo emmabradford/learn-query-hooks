e75df62044cec2b9e41c4ab045b72116c0cad08e
EmmaAbe <emma.abe.bradford@gmail.com>
2021-06-04 14:56:50 -0500

    added navbar code

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+    return(
+        <nav>
+            <button> Planetas</button>
+            <button> Personas</button>
+        </nav>
+    );
+}
+
+export default Navbar;
